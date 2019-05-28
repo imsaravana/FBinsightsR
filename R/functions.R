42541d18bd7e966be15881ce384acc12b661c049
imsaravana <imsaravana@live.com>
2019-05-28 13:51:35 -0400

    Update functions.R
    Updated filtering for active ads

--- R/functions.R
+++ R/functions.R
@@ -33,6 +33,7 @@ fbins_ag <- function(start_date, until_date, report_level, fb_access_token, acco
                   query = list(
                     access_token = fb_access_token,
                     time_range = time_range,
+                    filtering = "[{'field':'adset.effective_status','operator':'IN','value':['ACTIVE']}]",
                     level= report_level,
                     fields = "campaign_name, campaign_id, objective, adset_id, adset_name, ad_id, ad_name, impressions, cpm, reach, clicks, unique_clicks, ctr, cpc, unique_ctr, cost_per_unique_click, estimated_ad_recall_rate, cost_per_estimated_ad_recallers, spend, canvas_avg_view_time, canvas_avg_view_percent",
                     time_increment="1",
